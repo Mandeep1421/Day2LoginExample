a401ae4d6a2b53db5997da13f593ea3f52303e37
owner <samirthaker2020>
2019-03-29 14:59:13 -0400

    calculator design

--- app/src/main/java/com/example/day2loginexample/Home.java
+++ app/src/main/java/com/example/day2loginexample/Home.java
@@ -1,10 +1,27 @@
 package com.example.day2loginexample;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
+
+
+    private Button btnadd;
+    private Button btnsub;
+    private Button btnmul;
+    private Button btndiv;
+    private TextView txtno1;
+    private TextView txtno2;
+    private TextView msg;
+    private Double no1;
+    private Double no2;
+    private Double ans;
+private String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +36,32 @@ public class Home extends AppCompatActivity {
                 Toast.makeText(this,name,Toast.LENGTH_LONG).show();
             }
         }
+
+        btnadd=findViewById(R.id.btnadd);
+        btnsub=findViewById(R.id.btnminus);
+        btnmul=findViewById(R.id.MUL);
+        btndiv=findViewById(R.id.btndiv);
+        txtno1=findViewById(R.id.no1);
+        txtno2=findViewById(R.id.no2);
+        msg=findViewById(R.id.errormsg);
+
+
+
+        btnadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    no1 = Double.parseDouble ( txtno1.getText().toString());
+                    no2 = Double.parseDouble(txtno2.getText().toString());
+
+                    ans=no1+no2;
+                    msg.setText(ans.toString());
+
+            }
+        });
+
+
+
+
     }
 }
