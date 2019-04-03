92518acb6721467eabf6fcfc8e95b58b35d8c84e
samir-dell <samirthaker2020>
2019-04-03 16:50:07 -0400

    result display

--- app/src/main/java/com/example/day2loginexample/Result.java
+++ app/src/main/java/com/example/day2loginexample/Result.java
@@ -1,10 +1,8 @@
 package com.example.day2loginexample;
-import java.io.Serializable;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.day2loginexample.Modal.Student1;
 
@@ -12,6 +10,13 @@ public class Result extends AppCompatActivity {
 
 
     private TextView studentid;
+    private TextView studentname;
+    private TextView gender;
+    private TextView m1;
+    private TextView m2;
+    private TextView m3;
+    private TextView m4;
+    private TextView m5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,14 @@ public class Result extends AppCompatActivity {
 
      //   Intent i = getIntent();
         studentid=findViewById(R.id.rid);
+        studentname=findViewById(R.id.rname);
+        gender=findViewById(R.id.rgender);
+        m1=findViewById(R.id.smarks1);
+        m2=findViewById(R.id.smarks2);
+        m3=findViewById(R.id.smarks3);
+        m4=findViewById(R.id.smarks4);
+        m5=findViewById(R.id.smarks5);
+
 
 
       //
@@ -28,8 +41,14 @@ public class Result extends AppCompatActivity {
 
 
 
-studentid.setText(studobj.getSid());
-
+studentid.setText("Student ID:"+studobj.getSid());
+studentname.setText("Student Name:"+studobj.getSname());
+gender.setText("Gender:"+studobj.getGender());
+m1.setText("Marks1::"+studobj.getMarks()[0]);
+        m2.setText("Marks2::"+studobj.getMarks()[1]);
+        m3.setText("Marks3::"+studobj.getMarks()[2]);
+        m4.setText("Marks4::"+studobj.getMarks()[3]);
+        m5.setText("Marks5::"+studobj.getMarks()[4]);
         }
     }
 
